da947b10dee941f4bb71bce5d00670648ab83212
Chenxi Chen <chenxi.chen@thermofisher.com>
2021-05-28 11:28:40 -0700

    Updated "if restart" statement in main block by adding more synonyms

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -190,7 +190,7 @@ def main():
         user_stats(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
+        if restart.lower() not in ['yes', 'y', 'yup', 'yeah']:
             break
 
 
